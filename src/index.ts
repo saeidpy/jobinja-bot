ac8ccfd5a08cafee299ebbb8375914eee496ce20
Saeid Pourgohari <50409896+saeidpy@users.noreply.github.com>
2024-03-18 16:15:23 +0000

    feat: move to bot template

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,120 @@
-import { Telegraf } from 'telegraf';
+import { Markup, Telegraf } from 'telegraf';
 
-import { about } from './commands';
-import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
+import { greeting } from './text';
+import scrapper from './utils/scrapper';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
 const bot = new Telegraf(BOT_TOKEN);
+// Define variables for URL and keywords
+let url = "";
+let keywords: string[] = [];
 
-bot.command('about', about());
+// Define commands and their descriptions
+const commands = [
+  {
+    command: "/start",
+    description: "Start the bot and get a welcome message.",
+  },
+  {
+    command: "/seturl <URL>",
+    description: "Set the URL for scraping job details.",
+  },
+  {
+    command: "/setkeywords <keywords>",
+    description: "Set keywords for filtering job positions (comma-separated).",
+  },
+  {
+    command: "/scrape",
+    description:
+      "Trigger the scraping process based on the configured settings.",
+  },
+  {
+    command: "/setting",
+    description: "View of the settings.",
+  },
+  {
+    command: "/help",
+    description: "Get information about all available commands.",
+  },
+];
+
+// Handle /help command
+bot.command("help", (ctx) => {
+  try {
+    // Generate help message with commands and descriptions
+    const helpMessage = commands
+      .map((cmd) => `${cmd.command}: ${cmd.description}`)
+      .join("\n");
+    ctx.reply(`Available commands:\n\n${helpMessage}`);
+  } catch (error) {
+    console.error("Error:", error);
+    ctx.reply("An error occurred while fetching help information.");
+  }
+});
+
+// Handle /start command
+bot.command("start", (ctx) => {
+  ctx.reply(
+    "Welcome to the job scraper bot! Use /seturl to set the URL for scraping, /setkeywords to set keywords for filtering job positions.",
+    Markup.keyboard([["/scrape", "/setting", "/help"]])
+      .oneTime()
+      .resize()
+  );
+});
+
+// Handle /seturl command
+bot.command("seturl", (ctx) => {
+  const message = ctx.message.text.replace("/seturl", "").trim();
+  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+
+  if (!message) {
+    ctx.reply(`Please set the URL after the command`);
+    return;
+  }
+
+  if (!urlRegex.test(message)) {
+    ctx.reply(`Invalid URL format. Please provide a valid URL.`);
+    return;
+  }
+
+  url = message;
+  ctx.reply(`URL set: [LINK](${url})`, { parse_mode: "Markdown" });
+});
+
+// Handle /setkeywords command
+bot.command("setkeywords", (ctx) => {
+  const message = ctx.message.text.replace("/setkeywords", "").trim();
+  if (!message) {
+    ctx.reply(`Please set the keywords after the command`);
+    return;
+  }
+  keywords = message.split(",").map((keyword) => keyword.trim());
+  ctx.reply(`Keywords set to: ${keywords.join(", ")}`);
+});
+
+// Handle /scrape command
+bot.command("scrape", async (ctx) => {
+  try {
+    if (!url) {
+      return ctx.reply("Please set URL.");
+    }
+    ctx.reply(`please waite for scraping... `);
+
+    scrapper(url, keywords, ctx);
+  } catch (error) {
+    console.error("Error:", error);
+    ctx.reply("An error occurred while scraping job details.");
+  }
+});
+
+// Handle /start command
+bot.command("setting", (ctx) => {
+  ctx.reply(`URL set to: ${url}\nKeywords set to: ${keywords.join(", ")}`);
+});
 bot.on('message', greeting());
 
 //prod mode (Vercel)
---
+++ src/utils/scrapper.ts
@@ -0,0 +1,181 @@
+import axios from "axios";
+import cheerio from "cheerio";
+const JDate = require('jalali-date');
+import { Context, Telegraf } from "telegraf";
+import { Update } from "telegraf/typings/core/types/typegram";
+
+
+// Function to scrape the website and collect links
+async function scrapeAndCollectLinks(
+    _url: string,
+    page: number = 1,
+    collectedLinks: Set<string> = new Set()
+): Promise<string[]> {
+    try {
+        // Construct URL with page parameter
+        const url = `${_url}&page=${page}`;
+
+        // Fetch HTML from the URL
+        const response = await axios.get(url);
+        const html = response.data;
+
+        // Parse HTML using Cheerio
+        const $ = cheerio.load(html);
+
+        // Check if no results found
+        if ($("div.c-jobSearch__noResult").length > 0) {
+            console.log("No more results found. Stopping pagination.");
+            return Array.from(collectedLinks); // Convert Set to Array
+        }
+
+        // Extract links from the ul.c-jobListView__list
+        $("ul.c-jobListView__list")
+            .find("li")
+            .each((index, element) => {
+                const link = $(element)
+                    .find("div.o-listView__itemInfo > a")
+                    .attr("href");
+                if (link) {
+                    // Split the link at the fifth occurrence of '/'
+                    const parts = link.split("/");
+                    const modifiedLink = parts.slice(0, 6).join("/");
+                    collectedLinks.add(modifiedLink); // Add to Set to ensure uniqueness
+                }
+            });
+
+        // Continue pagination and collect links
+        return scrapeAndCollectLinks(_url, page + 1, collectedLinks);
+    } catch (error) {
+        console.error("Error:", error);
+        return Array.from(collectedLinks); // Convert Set to Array
+    }
+}
+
+// Function to scrape each link and collect data
+async function scrapeLink(_keywords: string[] = [], link: string) {
+    try {
+        // Fetch HTML from the link with a delay
+        await new Promise((resolve) => setTimeout(resolve, 500)); // Adjust the delay as needed
+        const response = await axios.get(link);
+        const html = response.data;
+        // Parse HTML using Cheerio
+        const $ = cheerio.load(html);
+
+        // Define keywords to filter positions
+        const keywords = _keywords;
+
+        // Extract data from each <li> element in the specified <ul>
+        const jobDetails = $(
+            "section:not([class]) ul.o-listView__list.c-jobListView__list li.o-listView__item"
+        )
+            .map((index, element) => {
+                const $element = $(element);
+                // Extract company name
+                const companyName = $element
+                    .find("div > div.o-listView__itemInfo > ul > li:nth-child(1) > span")
+                    .text()
+                    .trim();
+                // Extract position
+                const position = $element
+                    .find("div > div.o-listView__itemInfo > h2 > a")
+                    .text()
+                    .trim();
+                // Extract link
+                const jobLink = $element
+                    .find("div > div.o-listView__itemInfo > h2 > a")
+                    .attr("href");
+                // Check if position name includes any of the keywords
+                const filtered = keywords.some((keyword) =>
+                    position.toLowerCase().includes(keyword)
+                );
+                // Return job details if position matches any keyword
+                if (filtered) {
+                    return { companyName, position, jobLink };
+                }
+                return null;
+            })
+            .get()
+            .filter(Boolean); // Remove null values
+
+        return jobDetails.length ? jobDetails : []; // Return an empty array if no matching job details are found
+    } catch (error) {
+        console.error("Error scraping link:", error);
+        return []; // Return an empty array in case of error
+    }
+}
+
+function splitArrayChunks<T>(array: T[], n: number): T[][] {
+    const result: T[][] = [];
+    for (let i = 0; i < array.length; i += n) {
+        result.push(array.slice(i, i + n));
+    }
+    return result;
+}
+
+// Function to send collected job details to Telegram
+async function sendJobDetailsToTelegram(ctx: Context<Update>, jobDetails: {
+    companyName?: string;
+    position?: string;
+    jobLink?: string;
+}[]) {
+    const jdate = new JDate();
+    try {
+        if (jobDetails.length > 0) {
+            const messageHeader = `${jobDetails.length} Postion found\n`;
+            const nowDate = `Date: ${jdate.format("dddd DD MMMM YYYY")}\n\n`;
+            const chunksOfJobs = splitArrayChunks(jobDetails, 12);
+            const jobDetailsText = chunksOfJobs.map(
+                (jobDetails) =>
+                    messageHeader +
+                    nowDate +
+                    jobDetails
+                        .map(
+                            (job) =>
+                                `Company Name: ${job?.companyName}\nPosition: [${job?.position}](${job?.jobLink})\n`
+                        )
+                        .join("\n\n")
+            );
+
+            // Send each chunk as a separate message
+            for (const chunk of jobDetailsText) {
+                // Send message to Telegram chat
+                await ctx.reply( chunk, {
+                    parse_mode: "Markdown",
+                });
+            }
+
+            console.log("All job details sent successfully!");
+        } else {
+            console.log("No job details found.");
+        }
+    } catch (error) {
+        console.error("Error sending job details to Telegram:", error);
+    }
+}
+
+// Main function to orchestrate scraping and sending job details
+async function scrapper(url: string, keywords: string[], ctx: Context<Update>) {
+    try {
+        const collectedLinks = await scrapeAndCollectLinks(url);
+        console.log("🚀 ~ main ~ collectedLinks length:", collectedLinks.length);
+        // Loop through collected links and scrape job details
+        const jobDetails: {
+            companyName?: string;
+            position?: string;
+            jobLink?: string;
+        }[] = [];
+        for (const link of collectedLinks) {
+            const jobDetail = await scrapeLink(keywords, link);
+            if (jobDetail) {
+                jobDetails.push(...jobDetail);
+            }
+        }
+
+        // Send all job details to Telegram
+        await sendJobDetailsToTelegram(ctx, jobDetails);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+export default scrapper;

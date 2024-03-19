b961ec44315dd2d0dbf57b82349e061df8f5365b
Saeid Pourgohari <50409896+saeidpy@users.noreply.github.com>
2024-03-19 07:36:52 +0000

    fix: remove set hook automate

--- src/core/production.ts
+++ src/core/production.ts
@@ -6,7 +6,6 @@ import { Update } from 'telegraf/typings/core/types/typegram';
 const debug = createDebug('bot:dev');
 
 const PORT = (process.env.PORT && parseInt(process.env.PORT, 10)) || 3000;
-const VERCEL_URL = `${process.env.VERCEL_URL}`;
 
 const production = async (
   req: VercelRequest,
@@ -14,24 +13,12 @@ const production = async (
   bot: Telegraf<Context<Update>>,
 ) => {
   debug('Bot runs in production mode');
-  debug(`setting webhook: ${VERCEL_URL}`);
-
-  if (!VERCEL_URL) {
-    throw new Error('VERCEL_URL is not set.');
-  }
-
   const getWebhookInfo = await bot.telegram.getWebhookInfo();
-  if (getWebhookInfo.url !== VERCEL_URL + '/api') {
-    debug(`deleting webhook ${VERCEL_URL}`);
-    await bot.telegram.deleteWebhook();
-    debug(`setting webhook: ${VERCEL_URL}/api`);
-    await bot.telegram.setWebhook(`${VERCEL_URL}/api`);
-  }
 
   if (req.method === 'POST') {
     await bot.handleUpdate(req.body as unknown as Update, res);
   } else {
-    res.status(200).json(`Listening to bot events...\n starting webhook on port: ${PORT}\n vercel url: ${req.url}\n getWebhookInfo.url: ${getWebhookInfo.url}`);
+    res.status(200).json(`Listening to bot events... => getWebhookInfo.url: ${getWebhookInfo.url}`);
   }
   debug(`starting webhook on port: ${PORT}`);
 };

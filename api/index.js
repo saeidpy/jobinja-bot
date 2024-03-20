908679187e64e01daa9796dc062fc1bfa0b9a578
Saeid Pourgohari <50409896+saeidpy@users.noreply.github.com>
2024-03-20 15:54:12 +0000

    feat: add api index in js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,9 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
 import { startVercel } from '../src';
 
-export default async function handle(req: VercelRequest, res: VercelResponse) {
+export default async function handle(req, res) {
   try {
     await startVercel(req, res);
-  } catch (e: any) {
+  } catch (e) {
     res.statusCode = 500;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
